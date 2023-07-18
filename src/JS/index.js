084ddc0f90f1cbe1399fcb03246414362b50b131
AndrePort <aporto-@hotmail.com>
2023-07-17 21:28:55 -0300

    added more JS content and finished project

--- src/JS/index.js
+++ src/JS/index.js
@@ -2,12 +2,43 @@ const personagens = document.querySelectorAll('.personagem');
 
 personagens.forEach((personagem) => {
   personagem.addEventListener('mouseenter', () => {
-    const personagemSelecionado = document.querySelector('.selecionado');
+    //Modo celular
+    if(window.innerWidth < 450) {
+      window.scrollTo({top: 0, behavior: 'smooth'});
+    }
+    //Seleção de personagem
+    removerSelecaoDoPersonagem();
 
     personagem.classList.add('selecionado');
-    
-    personagemSelecionado.classList.remove('selecionado');
 
+    //Alteração imagem Personagem
+    alteracaoImagemPersonagemSelecionado(personagem);
 
+    //Alteração Titulo
+    alterarNomePersonagemSelecionado(personagem);
+
+    //Alteração descrição
+    alterarDescricaoPersonagemSelecionado(personagem);
   })
 })
+
+function alterarDescricaoPersonagemSelecionado(personagem) {
+  const descricaoPersonagem = document.getElementById('descricao-personagem');
+  descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+}
+
+function alterarNomePersonagemSelecionado(personagem) {
+  const nomePersonagem = document.getElementById('nome-personagem');
+  nomePersonagem.innerText = personagem.getAttribute('data-name');
+}
+
+function alteracaoImagemPersonagemSelecionado(personagem) {
+  const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+  const idPersonagem = personagem.attributes.id.value;
+  imagemPersonagemGrande.src = `./src/img/card-${idPersonagem}.png`;
+}
+
+function removerSelecaoDoPersonagem() {
+  const personagemSelecionado = document.querySelector('.selecionado');
+  personagemSelecionado.classList.remove('selecionado');
+}
